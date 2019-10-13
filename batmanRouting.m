ddf6be9b4945e3d6762562e7a0f6189afadc16b0
Wouter Kayser <wouterkayser@gmail.com>
2019-10-13 16:56:29 +0200

    Working routing part

---
+++ batmanRouting.m
@@ -0,0 +1,30 @@
+function metric = batmanRouting(batmanTable, connMatrix, traffic)
+
+    metric.numData = 0;
+    metric.success = 0;
+    
+    for i = 1:length(traffic)
+        start = traffic(i, 1);
+        destination = traffic(i, 2);
+        visited = 127 * ones(50, 1);
+        hopcount = batmanTable(destination, start);
+        visited(start) = hopcount;
+       
+        while hopcount > -1
+            current = find(visited == hopcount);
+            [neighbors, ~] = find(connMatrix(:, current));
+            metric.numData = metric.numData + length(current);
+            for j = 1:length(neighbors)
+                if batmanTable(destination, neighbors(j)) < hopcount
+                    visited(neighbors(j)) = batmanTable(destination, neighbors(j));
+                end
+            end
+            hopcount = hopcount - 1;
+        end
+        if visited(destination) < 127
+            metric.success = metric.success + 1;
+        end
+    end
+    metric.failure = length(traffic) - metric.success;
+end
+
