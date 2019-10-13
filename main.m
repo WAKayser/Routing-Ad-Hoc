0a646c3aea16f6430057443937fcac33de7facf6
Wouter Kayser <wouterkayser@gmail.com>
2019-10-13 17:11:41 +0200

    update sandbox to include batman protocol

--- main.m
+++ main.m
@@ -15,8 +15,9 @@ for t = 1:100
     results(t).oneHop = oneHopRouting(connMatrix, traffic);
     results(t).broadcast = broadcastRouting(connMatrix, traffic);
     results(t).ideal = idealRouting(connMatrix, traffic);
+    results(t).batmanR = batmanRouting(batmanTable, connMatrix, traffic);
     
     nodes = nodeMover(nodes);
     connMatrix = conUpdater(connMatrix, nodes, 2, 5);
-    [metric(t+1).batman, batmanTable] = batmanUpdater(batmanTable, connMatrix, nodes, 0.1, 0.1);
+    [metric(t+1).batmanU, batmanTable] = batmanUpdater(batmanTable, connMatrix, nodes, 0.1, 0.1);
 end
