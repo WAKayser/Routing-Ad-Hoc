8770d46703706a559017bbf5aab50596560e78d2
Wouter Kayser <wouterkayser@gmail.com>
2019-10-19 13:36:01 +0200

    routing based on existence of path

--- dsdvRouting.m
+++ dsdvRouting.m
@@ -5,17 +5,8 @@ function metric = dsdvRouting(dsdvTable, connMatrix, traffic)
     
     for i = 1:length(traffic)
        pathLength = dsdvTable(traffic(i, 1), traffic(i, 2), 2);
-       next = traffic(i, 1);
        if pathLength < inf
-           for n = 1:pathLength
-               previous = next;
-               next = dsdvTable(previous, traffic(i, 2), 1);               
-               metric.numData = metric.numData + 1;
-               if (next == 0) || (connMatrix(previous, next) == 0)
-                   metric.failure = metric.failure + 1;
-                   break
-               end
-           end
+           metric.numData = metric.numData + pathLength;
        else
            metric.failure = metric.failure + 1;
        end
