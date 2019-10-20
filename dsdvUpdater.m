54510d6f747efe34700f57edc736fb243c260fea
Wouter Kayser <wouterkayser@gmail.com>
2019-10-20 11:41:35 +0200

    Small cosmetic changes

--- dsdvUpdater.m
+++ dsdvUpdater.m
@@ -2,8 +2,9 @@ function [metric, routing] = dsdvUpdater(routing, connMatrix)
 
     metric.numRoute = 0;
     oldNumData = 0;
+    numNodes = length(connMatrix);
     
-    for n = 1:length(connMatrix)
+    for n = 1:numNodes
         routing(n, n, 1) = n;
         routing(n, n, 2) = 0;
         neighbors = find(connMatrix(:, n));
@@ -17,17 +18,14 @@ function [metric, routing] = dsdvUpdater(routing, connMatrix)
             oldNeighbors = [];
         end
 
-        for x = 1:length(oldNeighbors)
-            oldNeighbor = oldNeighbors(x);
-            routing(n, oldNeighbor, 1) = 0;
-            routing(n, oldNeighbor, 2) = inf;
-            routing(n, oldNeighbor, 3) = floor(routing(n, oldNeighbor, 3)/2)*2 + 1;
+        routing(n, oldNeighbors, 1) = 0;
+        routing(n, oldNeighbors, 2) = inf;
+        routing(n, oldNeighbors, 3) = floor(routing(n, oldNeighbors, 3)/2)*2 + 1;
 
-            routing(oldNeighbor, n ,1) = 0;
-            routing(oldNeighbor, n ,2) = inf;
-            routing(oldNeighbor, n, 3) = floor(routing(oldNeighbor, n, 3)/2)*2 + 1;
-            metric.numRoute = metric.numRoute + 2;
-        end
+        routing(oldNeighbors, n ,1) = 0;
+        routing(oldNeighbors, n ,2) = inf;
+        routing(oldNeighbors, n, 3) = floor(routing(oldNeighbors, n, 3)/2)*2 + 1;
+        metric.numRoute = metric.numRoute + 2*length(oldNeighbors);
     end
     
     while(metric.numRoute ~= oldNumData)
@@ -35,13 +33,11 @@ function [metric, routing] = dsdvUpdater(routing, connMatrix)
         for n = 1:length(connMatrix)
             for entry = 1:length(connMatrix)
                 step = routing(n, entry, 1);
-                if step
-                    if mod(routing(step, entry, 3), 2)
-                        metric.numRoute = metric.numRoute + 1;
-                        routing(n, entry, 1) = 0;
-                        routing(n, entry, 2) = inf;
-                        routing(n, entry, 3) = routing(step, entry, 3) + 2;
-                    end
+                if step && mod(routing(step, entry, 3), 2)
+                    metric.numRoute = metric.numRoute + 1;
+                    routing(n, entry, 1) = 0;
+                    routing(n, entry, 2) = inf;
+                    routing(n, entry, 3) = routing(step, entry, 3) + 2;
                 end
             end
         end
